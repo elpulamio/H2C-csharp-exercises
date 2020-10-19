bc924785b7e0619423e438bcac54cf0d22de1cbe
elpulamio <lifedowner@gmail.com>
2020-10-19 12:38:20 +0300

    19.10.2020

--- Exercises/Part 1/Exercise 016/Program.cs
+++ Exercises/Part 1/Exercise 016/Program.cs
@@ -7,7 +7,12 @@ namespace exercise_16
     public static void Main(string[] args)
     {
 
-      // Write your code here:
+      Console.WriteLine("How many days?");
+      string uInput = Console.ReadLine();
+      int days = Convert.ToInt32(uInput);
+      //seconds = days * hours per day * minutes per hour * seconds per minute
+      int seconds = days * 24 * 60 * 60;
+      Console.WriteLine(days + " days is " + seconds + " seconds.");
 
     }
   }
