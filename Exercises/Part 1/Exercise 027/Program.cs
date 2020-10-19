39b33d23ef66eb596be96112eceed6f404637ee2
elpulamio <lifedowner@gmail.com>
2020-10-19 15:53:16 +0300

    19.10.2020

--- Exercises/Part 1/Exercise 027/Program.cs
+++ Exercises/Part 1/Exercise 027/Program.cs
@@ -6,7 +6,17 @@ namespace exercise_27
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+      Console.WriteLine("Give a number:");
+      string nro = Console.ReadLine();
+      int nroInt = Convert.ToInt32(nro);
+      if (nroInt > 0)
+      {
+        Console.WriteLine("It is positive.");      
+      }
+      else
+      {
+        Console.WriteLine("It is not positive.");      
+      }
 
     }
   }
