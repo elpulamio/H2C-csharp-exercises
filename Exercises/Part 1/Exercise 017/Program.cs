82439d215fa10db12e58dbac06f74a6d5f9a0195
elpulamio <lifedowner@gmail.com>
2020-10-19 13:04:01 +0300

    19.10.2020

--- Exercises/Part 1/Exercise 017/Program.cs
+++ Exercises/Part 1/Exercise 017/Program.cs
@@ -7,8 +7,12 @@ namespace exercise_17
     public static void Main(string[] args)
     {
 
-      // Write your code here:
-
+      Console.WriteLine("Give the first number!");
+      string firstNro = Console.ReadLine();
+      Console.WriteLine("Give the second number!");
+      string secondNro = Console.ReadLine();
+      Console.WriteLine("The sum is " + (Convert.ToInt32(firstNro) + Convert.ToInt32(secondNro)));
+      
     }
   }
 }
