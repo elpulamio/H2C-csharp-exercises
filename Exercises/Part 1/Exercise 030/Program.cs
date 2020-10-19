be1ec6b475453737044b7702179cfe9eba8390fd
elpulamio <lifedowner@gmail.com>
2020-10-19 22:29:46 +0300

    19.10.2020

--- Exercises/Part 1/Exercise 030/Program.cs
+++ Exercises/Part 1/Exercise 030/Program.cs
@@ -6,8 +6,42 @@ namespace exercise_30
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
-
+      Console.WriteLine("Give your percent [0 - 100]:");
+      string percent = Console.ReadLine();
+      int pInt = Convert.ToInt32(percent);
+      
+      if (pInt < 0)
+      {
+        Console.WriteLine("Impossible");
+      }
+      else if (pInt > -1 && pInt < 50)
+      {
+        Console.WriteLine("Fail");
+      }
+      else if (pInt > 49 && pInt < 60)
+      {
+        Console.WriteLine("1");
+      }
+      else if (pInt > 59 && pInt < 70)
+      {
+        Console.WriteLine("2");
+      }
+      else if (pInt > 69 && pInt < 80)
+      {
+        Console.WriteLine("3");
+      }
+      else if (pInt > 79 && pInt < 90)
+      {
+        Console.WriteLine("4");
+      }
+      else if (pInt > 89 && pInt < 101)
+      {
+        Console.WriteLine("5");
+      }
+      else
+      {
+        Console.WriteLine("Outstanding!");      
+      }
     }
   }
 }
