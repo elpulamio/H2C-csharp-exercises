bcb9cba555776affb5c5454ea21157e2346c3be6
elpulamio <lifedowner@gmail.com>
2020-10-19 22:54:54 +0300

    19.10.2020

--- Exercises/Part 1/Exercise 033/Program.cs
+++ Exercises/Part 1/Exercise 033/Program.cs
@@ -6,7 +6,18 @@ namespace exercise_33
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+      Console.WriteLine("Give the first string:");
+      string message = Console.ReadLine();
+      Console.WriteLine("Give the second string:");
+      string message2 = Console.ReadLine();
+      if (message == message2)
+      {
+        Console.WriteLine("Echo!");
+      }
+      else
+      {
+        Console.WriteLine("Nope!");
+      }
 
     }
   }
