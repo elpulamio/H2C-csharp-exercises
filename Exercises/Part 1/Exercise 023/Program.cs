04d152e731db9139aa0056600367d8690e1b1b5b
elpulamio <lifedowner@gmail.com>
2020-10-19 14:39:54 +0300

    19.10.2020

--- Exercises/Part 1/Exercise 023/Program.cs
+++ Exercises/Part 1/Exercise 023/Program.cs
@@ -7,8 +7,20 @@ namespace exercise_23
     public static void Main(string[] args)
     {
 
-      // Write your code here:
-
+      Console.WriteLine("Give the first number!");
+      string firstNro = Console.ReadLine();
+      Console.WriteLine("Give the second number!");
+      string secondNro = Console.ReadLine();
+      int Nro1 = Convert.ToInt32(firstNro);
+      int Nro2 = Convert.ToInt32(secondNro);
+      int sum = Nro1 + Nro2;
+      int sub = Nro1 - Nro2;
+      int multi = Nro1 * Nro2;
+      double divide = Convert.ToDouble(Nro1) / Convert.ToDouble(Nro2);
+      Console.WriteLine(Nro1 + " + " + Nro2 + " = " + sum);
+      Console.WriteLine(Nro1 + " - " + Nro2 + " = " + sub);
+      Console.WriteLine(Nro1 + " * " + Nro2 + " = " + multi);
+      Console.WriteLine(Nro1 + " / " + Nro2 + " = " + divide);
     }
   }
 }
