c030591cc40a9398a4afbcbeb18702c36316cda1
elpulamio <lifedowner@gmail.com>
2020-10-19 13:37:31 +0300

    19.10.2020

--- Exercises/Part 1/Exercise 018/Program.cs
+++ Exercises/Part 1/Exercise 018/Program.cs
@@ -7,7 +7,13 @@ namespace exercise_18
     public static void Main(string[] args)
     {
 
-      // Write your code here:
+      Console.WriteLine("Give the first number!");
+      string firstNro = Console.ReadLine();
+      Console.WriteLine("Give the second number!");
+      string secondNro = Console.ReadLine();
+      Console.WriteLine("Give the third number!");
+      string thirdNro = Console.ReadLine();
+      Console.WriteLine("The sum is " + (Convert.ToInt32(firstNro) + Convert.ToInt32(secondNro) + Convert.ToInt32(thirdNro)));
 
     }
   }
