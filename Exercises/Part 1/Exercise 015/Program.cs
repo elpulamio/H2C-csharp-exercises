fedf50d378506cc380491bedc7748ba7858f14bf
elpulamio <lifedowner@gmail.com>
2020-10-19 11:22:42 +0300

    19.10.2020

--- Exercises/Part 1/Exercise 015/Program.cs
+++ Exercises/Part 1/Exercise 015/Program.cs
@@ -7,7 +7,19 @@ namespace exercise_15
     public static void Main(string[] args)
     {
 
-      // Write your code here:
+      Console.WriteLine("Give a string:");
+      string message = Console.ReadLine();
+      Console.WriteLine("Give an integer:");
+      string numberStr = Console.ReadLine();
+      Console.WriteLine("Give a double:");
+      string dNumberStr = Console.ReadLine();
+      Console.WriteLine("Give a boolean:");
+      string boolStr = Console.ReadLine();
+      
+      Console.WriteLine("Your string: " + message);
+      Console.WriteLine("Your integer: " + Convert.ToInt32(numberStr));
+      Console.WriteLine("Your double: " + Convert.ToDouble(dNumberStr));
+      Console.WriteLine("Your boolean: " + Convert.ToBoolean(boolStr));
 
     }
   }
