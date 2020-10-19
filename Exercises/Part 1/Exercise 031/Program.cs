7b26ef4cf56a204751cb8c10d2f8f39c444c4666
elpulamio <lifedowner@gmail.com>
2020-10-19 22:41:11 +0300

    19.10.2020

--- Exercises/Part 1/Exercise 031/Program.cs
+++ Exercises/Part 1/Exercise 031/Program.cs
@@ -6,7 +6,18 @@ namespace exercise_31
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+      Console.WriteLine("Give a number:");
+      string number = Console.ReadLine();
+      int nro = Convert.ToInt32(number);
+      int rem = nro % 2;
+      if (rem == 0)
+      {
+        Console.WriteLine("It is even.");      
+      }
+      else
+      {
+        Console.WriteLine("It is odd.");
+      }
 
     }
   }
