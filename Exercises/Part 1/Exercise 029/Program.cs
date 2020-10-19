ccb3439cf6d619ecb186e067e89ffd6fd2d2ca05
elpulamio <lifedowner@gmail.com>
2020-10-19 21:52:50 +0300

    19.10.2020

--- Exercises/Part 1/Exercise 029/Program.cs
+++ Exercises/Part 1/Exercise 029/Program.cs
@@ -6,7 +6,24 @@ namespace exercise_29
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+      Console.WriteLine("Give the first number!");
+      string nro = Console.ReadLine();
+      int nroInt = Convert.ToInt32(nro);
+      Console.WriteLine("Give the second number!");
+      string nro2 = Console.ReadLine();
+      int nro2Int = Convert.ToInt32(nro2);
+      if (nroInt > nro2Int)
+      {
+        Console.WriteLine("The larger number is " + nroInt + "!");      
+      }
+      else if (nroInt < nro2Int)
+      {
+        Console.WriteLine("The larger number is " + nro2Int + "!");      
+      }
+      else
+      {
+        Console.WriteLine("They are equal!");      
+      }
 
     }
   }
