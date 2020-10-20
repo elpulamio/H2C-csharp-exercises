c676a7e5b02f50e0447bfd88634f8ee141df34b3
elpulamio <lifedowner@gmail.com>
2020-10-20 18:18:16 +0300

    20.10.2020

--- Exercises/Part 1/Exercise 036/Program.cs
+++ Exercises/Part 1/Exercise 036/Program.cs
@@ -6,8 +6,23 @@ namespace exercise_36
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
-
+      int nro = 1;
+      string msg = "-";
+      while (nro != 0)
+      {
+                Console.WriteLine("Give a number:");
+                msg = Console.ReadLine();
+                nro = Convert.ToInt32(msg);
+                if (nro > 0)
+                {
+                    nro = nro * nro;
+                    Console.WriteLine(nro);
+                }
+                else if (nro < 0)
+                {
+                    Console.WriteLine("That is negative");
+                }
+      }
     }
   }
 }
