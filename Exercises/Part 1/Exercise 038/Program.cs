3816182d93c726ca2bf2fc72fb69dcb527783aa0
elpulamio <lifedowner@gmail.com>
2020-10-20 18:42:53 +0300

    20.10.2020

--- Exercises/Part 1/Exercise 038/Program.cs
+++ Exercises/Part 1/Exercise 038/Program.cs
@@ -6,8 +6,20 @@ namespace exercise_38
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
-
+      int sum = 0;
+      int nro = 1;
+      string msg = "-";
+      while (nro != 0)
+      {
+                Console.WriteLine("Give a number:");
+                msg = Console.ReadLine();
+                nro = Convert.ToInt32(msg);
+                if (nro < 0)
+                {
+                    sum = sum + 1;
+                }
+      }
+      Console.WriteLine("Total amount of negative numbers: " + sum);
     }
   }
 }
