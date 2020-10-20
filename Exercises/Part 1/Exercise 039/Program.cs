91077e2564304d6040e880418fe0d2c13a2948e5
elpulamio <lifedowner@gmail.com>
2020-10-20 18:50:59 +0300

    20.10.2020

--- Exercises/Part 1/Exercise 039/Program.cs
+++ Exercises/Part 1/Exercise 039/Program.cs
@@ -6,7 +6,20 @@ namespace exercise_39
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+      int sum = 0;
+      int nro = 1;
+      string msg = "-";
+      while (nro != 0)
+      {
+                Console.WriteLine("Give a number:");
+                msg = Console.ReadLine();
+                nro = Convert.ToInt32(msg);
+                if (nro != 0)
+                {
+                    sum = sum + nro;
+                }
+      }
+      Console.WriteLine("Total sum of numbers: " + sum);
     }
   }
 }
