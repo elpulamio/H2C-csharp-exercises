e9d060650df668014d7a619312fa3f5e713f2b3f
elpulamio <lifedowner@gmail.com>
2020-10-22 00:20:36 +0300

    22.10.2020

--- Exercises/Part 2/Exercise 048/Program.cs
+++ Exercises/Part 2/Exercise 048/Program.cs
@@ -6,9 +6,39 @@ namespace exercise_48
   {
     public static void Main(string[] args)
     {
-
-      // Write your code here:
- 
+      Console.WriteLine("Give numbers:");
+      int nro = 0;
+      int sum = 0;
+      int count = 0;
+      int even = 0;
+      int odd = 0;
+      int calc = 0;
+      
+      while (nro != -1)
+      {
+        nro = Convert.ToInt32(Console.ReadLine());
+        calc = nro % 2;
+        if (nro != -1)
+        {
+            sum = sum + nro;
+            count++;
+            if (calc == 0)
+            {
+                even++;        
+            }
+            else
+            {
+                odd++;       
+            }
+        }
+      }
+      
+      Console.WriteLine("Thx! Bye!");
+      Console.WriteLine("Sum: " + sum);
+      Console.WriteLine("Numbers: " + count);
+      Console.WriteLine("Average: " + (double)sum / count);
+      Console.WriteLine("Even: " + even);
+      Console.WriteLine("Odd: " + odd);
     }
   }
 }
