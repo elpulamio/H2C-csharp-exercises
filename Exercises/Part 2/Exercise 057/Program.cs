df06cb09734d79f03cfb06f953db0bce1883a712
elpulamio <lifedowner@gmail.com>
2020-10-24 17:09:14 +0300

    24.10.2020

--- Exercises/Part 2/Exercise 057/Program.cs
+++ Exercises/Part 2/Exercise 057/Program.cs
@@ -6,8 +6,8 @@ namespace exercise_57
   {
     public static int Sum(int number1, int number2, int number3, int number4)
     {
-      // write your code here
-      // remember to include return (at the end)!
+        int sum = number1 + number2 + number3 + number4;
+        return sum;
     }
 
     public static void Main(String[] args)
