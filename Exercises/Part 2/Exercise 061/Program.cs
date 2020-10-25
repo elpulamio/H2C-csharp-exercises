3aaaab5f3b2a3a6e9588b87317c6b7ac3f5353ae
elpulamio <lifedowner@gmail.com>
2020-10-25 15:57:55 +0200

    25.10.2020

--- Exercises/Part 2/Exercise 061/Program.cs
+++ Exercises/Part 2/Exercise 061/Program.cs
@@ -6,27 +6,71 @@ namespace exercise_61
   {
     public static void Main(String[] args)
     {
-
+        PrintRightTriangle(4);
+        Console.WriteLine("");
+        ChristmasTree(20);
     }
 
     public static void PrintStars(int number)
     {
-
+        for (int i = 0; i < number; i++)
+        {
+            Console.Write("*");
+        }
     }
 
     public static void PrintSpaces(int number)
     {
-
+        for (int i = 0; i < number; i++)
+        {
+            Console.Write(" ");
+        }
     }
 
     public static void PrintRightTriangle(int size)
     {
-
+        int e = size;
+        int x = 1;
+        for (int i = 0; i < size; i++)
+        {
+            e = e - 1;
+            PrintSpaces(e);
+            PrintStars(x);
+            Console.WriteLine("");
+            x++;
+        }
     }
 
     public static void ChristmasTree(int height)
     {
+        int width = -1;
+        int count = 0;
+        while (count < height)
+        {
+            width = width + 2;
+            count++;
+        }
 
+        int midpoint = width / 2;
+        int midpointStart = midpoint - 1;
+        int x = 1;
+        for (int i = 0; i < height; i++)
+        {
+            PrintSpaces(midpoint);
+            PrintStars(x);
+            PrintSpaces(midpoint);
+            x = x + 2;
+            midpoint = midpoint - 1;
+            Console.WriteLine("");
+        }
+        PrintSpaces(midpointStart);
+        PrintStars(3);
+        PrintSpaces(midpointStart);
+        Console.WriteLine("");
+        PrintSpaces(midpointStart);
+        PrintStars(3);
+        PrintSpaces(midpointStart);
+        Console.WriteLine("");
     }
   }
 }
