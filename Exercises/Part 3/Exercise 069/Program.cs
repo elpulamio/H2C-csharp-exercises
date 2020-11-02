ac7c86d8ced54d38d878a9e8d871463cc7baa573
elpulamio <lifedowner@gmail.com>
2020-11-02 19:01:06 +0200

    2.11.2020

--- Exercises/Part 3/Exercise 069/Program.cs
+++ Exercises/Part 3/Exercise 069/Program.cs
@@ -17,7 +17,18 @@ namespace exercise_69
         }
         list.Add(input);
       }
-
+      Console.WriteLine("From where?");
+      int start = Convert.ToInt32(Console.ReadLine());
+      Console.WriteLine("Where to?");
+      int end = Convert.ToInt32(Console.ReadLine());
+      int x = list.Count;
+      for (int i = 0; i < x; i++)
+      {
+        if (list[i] >= start && list[i] <= end)
+        {
+            Console.WriteLine(list[i]);
+        }
+      }
     }
   }
 }
