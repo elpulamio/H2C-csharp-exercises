5b09eb1ab0022be35ca0c360c51b9e986d47ad6a
elpulamio <lifedowner@gmail.com>
2020-11-06 13:22:32 +0200

    6.11.2020

--- Exercises/Part 3/Exercise 076/Program.cs
+++ Exercises/Part 3/Exercise 076/Program.cs
@@ -5,9 +5,28 @@ namespace exercise_76
 {
   class Program
   {
+    public static int Sum(List<int> numbers)
+    {
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum = sum + number;
+        }
+        return sum;
+    }
+
     public static void Main(string[] args)
     {
+        List<int> numbers = new List<int>();
+        numbers.Add(3);
+        numbers.Add(2);
+        numbers.Add(6);
+        numbers.Add(-1);
+        Console.WriteLine(Sum(numbers));
 
+        numbers.Add(5);
+        numbers.Add(1);
+        Console.WriteLine(Sum(numbers));
     }
 
   }
