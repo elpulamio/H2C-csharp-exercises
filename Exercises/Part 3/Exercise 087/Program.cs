f83949ec3ea25fdd14336d363553f32886509b94
elpulamio <lifedowner@gmail.com>
2020-11-13 12:26:02 +0200

    13.11.2020

--- Exercises/Part 3/Exercise 087/Program.cs
+++ Exercises/Part 3/Exercise 087/Program.cs
@@ -7,7 +7,23 @@ namespace exercise_87
   {
     public static void Main(string[] args)
     {
-
+        while (true)
+        {
+            string sentence = Console.ReadLine();
+            if (sentence == "")
+            {
+                break;
+            }
+            
+            string[] words = sentence.Split(' ');
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i].Contains("av"))
+                {
+                    Console.WriteLine(words[i]);
+                }
+            }
+        }
     }
 
   }
