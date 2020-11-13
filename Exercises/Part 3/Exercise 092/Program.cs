d6fc26fceab52b0b0c269f482c5e46ee282c7156
elpulamio <lifedowner@gmail.com>
2020-11-13 13:43:53 +0200

    13.11.2020

--- Exercises/Part 3/Exercise 092/Program.cs
+++ Exercises/Part 3/Exercise 092/Program.cs
@@ -7,7 +7,32 @@ namespace exercise_92
   {
     public static void Main(string[] args)
     {
-
+        string name = "";
+        int age = -1;
+        int height = 0;
+        while (true)
+        {
+            string person = Console.ReadLine();
+            if (person == "")
+            {
+                break;
+            }
+            
+            string[] info = person.Split(',');
+            
+            if (info[0].Length > height)
+            {
+                height = info[0].Length;
+                name = info[0];
+            }
+            
+            if ((2020 - Convert.ToInt32(info[1])) > age)
+            {
+                age  = 2020 - Convert.ToInt32(info[1]);
+            }
+        }
+        Console.WriteLine("Longest name: " + name);
+        Console.WriteLine("Highest age: " + age);
     }
   }
 }
