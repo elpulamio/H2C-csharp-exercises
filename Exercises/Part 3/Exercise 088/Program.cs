a2b715ca93622a61e38dd36eda3bcc3535997a22
elpulamio <lifedowner@gmail.com>
2020-11-13 12:29:48 +0200

    13.11.2020

--- Exercises/Part 3/Exercise 088/Program.cs
+++ Exercises/Part 3/Exercise 088/Program.cs
@@ -7,7 +7,17 @@ namespace exercise_88
   {
     public static void Main(string[] args)
     {
-
+        while (true)
+        {
+            string sentence = Console.ReadLine();
+            if (sentence == "")
+            {
+                break;
+            }
+            
+            string[] words = sentence.Split(' ');
+            Console.WriteLine(words[0]);
+        }
     }
   }
 }
