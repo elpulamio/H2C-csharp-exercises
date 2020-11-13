b3b5cf2d0a908566ba522ed0c3a669f965501dcd
elpulamio <lifedowner@gmail.com>
2020-11-13 09:26:14 +0200

    13.11.2020

--- Exercises/Part 3/Exercise 081/Program.cs
+++ Exercises/Part 3/Exercise 081/Program.cs
@@ -8,12 +8,25 @@ namespace exercise_81
     public static void Main(string[] args)
     {
       // You can test your method here
-
+      int[] array = {5, 1, 3, 4, 2};
+      PrintNeatly(array);
     }
 
     public static void PrintNeatly(int[] array)
     {
-
+        for (int i = 0; i < array.Length; i++) 
+        {
+            Console.Write(array[i]);
+            if ((i+1) < array.Length)
+            {
+                Console.Write(", ");
+            }
+            else
+            {
+                //To get the console info text (Press any key...) at the end into next line
+                Console.WriteLine();    
+            }
+        }
     }
   }
 }
