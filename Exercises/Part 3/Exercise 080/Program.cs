52cc255274f07839672e8dad647bee55b27ffd5b
elpulamio <lifedowner@gmail.com>
2020-11-13 09:05:22 +0200

    13.11.2020

--- Exercises/Part 3/Exercise 080/Program.cs
+++ Exercises/Part 3/Exercise 080/Program.cs
@@ -14,8 +14,12 @@ namespace exercise_80
     }
 
     public static int SumOfNumbersInArray(int[] numbers) {
-
-      return 0;
+        int sum = 0;
+        for (int i = 0; i < numbers.Length; i++) 
+        {
+            sum = sum + numbers[i];    
+        }
+        return sum;
     }
 
   }
