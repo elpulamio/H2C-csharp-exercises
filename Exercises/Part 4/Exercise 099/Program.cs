a338cf03f322d6f9735709e8b85f87e48be48250
elpulamio <lifedowner@gmail.com>
2020-11-26 11:47:38 +0200

    26.11.2020

--- Exercises/Part 4/Exercise 099/Program.cs
+++ Exercises/Part 4/Exercise 099/Program.cs
@@ -7,11 +7,31 @@ namespace exercise_99
     public static void Main(string[] args)
     {
 
-      DecreasingCounter counter = new DecreasingCounter(20);
+      DecreasingCounter counter = new DecreasingCounter(5);
       counter.PrintValue();
 
-      counter.Reset();
+      counter.Decrement();
       counter.PrintValue();
+
+      counter.Decrement();
+      counter.PrintValue();
+
+      counter.Decrement();
+      counter.PrintValue();
+
+      counter.Decrement();
+      counter.PrintValue();
+
+      counter.Decrement();
+      counter.PrintValue();
+
+      counter.Decrement();
+      counter.PrintValue();
+
+      DecreasingCounter counter2 = new DecreasingCounter(10);
+      counter2.PrintValue();
+      counter2.Reset();
+      counter2.PrintValue();
     }
   }
 }
