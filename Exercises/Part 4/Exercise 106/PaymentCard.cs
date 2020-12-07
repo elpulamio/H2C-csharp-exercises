0a22f2da73c20a975f5eaf4682c366df7ab6bb1d
elpulamio <lifedowner@gmail.com>
2020-12-07 10:23:53 +0200

    07.12.2020

---
+++ Exercises/Part 4/Exercise 106/PaymentCard.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace exercise_106
+{
+    class PaymentCard
+    {
+        private double balance;
+
+        public PaymentCard(double openingBalance)
+        {
+            // write code here
+            this.balance = openingBalance;
+        }
+
+        public override string ToString()
+        {
+            // write code here
+            return "The card has a balance of " + this.balance + " euros";
+        }
+    }
+}
