636cba9d26b79b1ad542ca897f791f769ecbdc77
elpulamio <lifedowner@gmail.com>
2020-12-07 13:25:08 +0200

    07.12.2020

--- Exercises/Part 4/Exercise 111/Program.cs
+++ Exercises/Part 4/Exercise 111/Program.cs
@@ -7,9 +7,30 @@ namespace exercise_111
   {
     public static void Main(string[] args)
     {
-      // IMPLEMENT YOUR CODE IN HERE!
-      List<PersonalInformation> list = new List<PersonalInformation>();
+        // IMPLEMENT YOUR CODE IN HERE!
+        List<PersonalInformation> list = new List<PersonalInformation>();
 
+        while (true)
+        {
+            Console.WriteLine("First name (empty stops the app: ");
+            String firstName = Console.ReadLine();
+            if (firstName == "")
+            {
+                break;
+            }
+            Console.WriteLine("Last name: ");
+            String lastName = Console.ReadLine();
+            Console.WriteLine("Identification number: ");
+            String identificationNumber = Console.ReadLine();
+            
+            list.Add(new PersonalInformation(firstName, lastName, identificationNumber));
+        }
+        Console.WriteLine();
+
+        foreach (PersonalInformation item in list)
+        {
+            Console.WriteLine(item.firstName + " " + item.lastName);
+        }
     }
   }
 }
