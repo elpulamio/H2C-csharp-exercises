c30b85bb57edca8e7d19f3f685b0ef6481eaedae
elpulamio <lifedowner@gmail.com>
2020-12-08 15:16:57 +0200

    08.12.2020

--- Exercises/Part 4/Exercise 112/Program.cs
+++ Exercises/Part 4/Exercise 112/Program.cs
@@ -7,9 +7,34 @@ namespace exercise_112
   {
     public static void Main(string[] args)
     {
-      // IMPLEMENT YOUR CODE IN HERE!
+        List<TelevisionProgram> list = new List<TelevisionProgram>();
 
+        String name = "";
+        int duration = 0;
 
+        while (true)
+        {
+            Console.WriteLine("Name (empty stops the app: ");
+            name = Console.ReadLine();
+            if (name == "")
+            {
+                break;
+            }
+            Console.WriteLine("Duration: ");
+            duration = Convert.ToInt32(Console.ReadLine());
+            
+            list.Add(new TelevisionProgram(name, duration));
+        }
+        Console.WriteLine();
+        Console.WriteLine("Program's maximum duration?");
+        int max = Convert.ToInt32(Console.ReadLine());
+            
+        foreach (TelevisionProgram item in list)
+        {
+            if (item.duration <= max){ 
+                Console.WriteLine(item);
+            }
+        }
     }
   }
 }
