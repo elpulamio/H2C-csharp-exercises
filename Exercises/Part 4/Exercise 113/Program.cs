6f40cf690743cfc001e6158c6269f4ba588b41b9
elpulamio <lifedowner@gmail.com>
2020-12-08 15:56:19 +0200

    08.12.2020

--- Exercises/Part 4/Exercise 113/Program.cs
+++ Exercises/Part 4/Exercise 113/Program.cs
@@ -7,9 +7,45 @@ namespace exercise_113
   {
     public static void Main(string[] args)
     {
-      // IMPLEMENT YOUR CODE IN HERE!
-      // Remember also to create the file Book.cs and class in it!
+        List<Book> list = new List<Book>();
 
+        String title = "";
+        int pages = 0;
+        int year = 0;
+
+        while (true)
+        {
+            Console.Write("Name: ");
+            title = Console.ReadLine();
+            if (title == "")
+            {
+                break;
+            }
+            Console.Write("Pages: ");
+            pages = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Publication year: ");
+            year = Convert.ToInt32(Console.ReadLine());
+            
+            list.Add(new Book(title, pages, year));
+        }
+        Console.WriteLine();
+        Console.Write("What information will be printed?: ");
+        String answer = Console.ReadLine();
+         
+        if (answer == "everything")
+        {
+            foreach (Book item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }   
+        else if (answer == "title")
+        {
+            foreach (Book item in list)
+            {
+                Console.WriteLine(item.title);
+            }
+        }
     }
   }
 }
