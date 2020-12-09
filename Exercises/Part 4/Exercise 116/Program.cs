9529fd9c4950db7846adc50ff3e8b47e0fd4c4fd
elpulamio <lifedowner@gmail.com>
2020-12-09 18:48:37 +0200

    09.12.2020

--- Exercises/Part 4/Exercise 116/Program.cs
+++ Exercises/Part 4/Exercise 116/Program.cs
@@ -10,6 +10,13 @@ namespace exercise_116
       // Read the file data.txt and print the text from it as it is
       // You can use either File.ReadAllText or File.ReadAllLines
       
+      //Could not get loaded the data.txt -file in project folder
+      //I tried to manipulate .csproj -file and many many other tricks,
+      //but nothing worked. Always get the path not found -error.
+      //So, I just use local file here:
+      string data = File.ReadAllText("c:/C#exercises/data.txt");
+      Console.WriteLine(data);
+
     }
   }
 }
