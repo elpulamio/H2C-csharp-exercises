f498d0d92c4a533793521b0711ccf7c9bf4d80be
elpulamio <lifedowner@gmail.com>
2020-12-09 12:07:47 +0200

    09.12.2020

--- Exercises/Part 4/Exercise 115/Program.cs
+++ Exercises/Part 4/Exercise 115/Program.cs
@@ -6,7 +6,19 @@ namespace exercise_115
   {
     public static void Main(string[] args)
     {
-
+        String input = "";
+        int number = 0;
+        
+        while (true)
+        {
+            input = Console.ReadLine();
+            if (input == "end")
+            {
+                break;   
+            }
+            number = Convert.ToInt32(input);
+            Console.WriteLine(number * number * number);   
+        }
     }
   }
 }
