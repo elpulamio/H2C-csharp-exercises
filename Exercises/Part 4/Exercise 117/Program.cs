751753b799bc3d2e28909d7471248f5c449cee85
elpulamio <lifedowner@gmail.com>
2020-12-10 11:41:18 +0200

    10.12.2020

--- Exercises/Part 4/Exercise 117/Program.cs
+++ Exercises/Part 4/Exercise 117/Program.cs
@@ -7,9 +7,13 @@ namespace exercise_117
   {
     public static void Main(string[] args)
     {
-      // Ask the user for the file name and print the content of the file
-
-      
+        // Ask the user for the file name and print the content of the file
+        Console.WriteLine("Which file should have its contents printed?");
+        string file = Console.ReadLine();
+        string data = File.ReadAllText("../../" + file);
+        Console.WriteLine();
+        Console.WriteLine(data);
+        Console.WriteLine();
     }
   }
 }
