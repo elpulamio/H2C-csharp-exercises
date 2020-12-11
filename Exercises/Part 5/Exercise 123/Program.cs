ea74e556fdfe711a4a6c0d3b4d6046c3680521e0
elpulamio <lifedowner@gmail.com>
2020-12-11 19:27:45 +0200

    11.12.2020

--- Exercises/Part 5/Exercise 123/Program.cs
+++ Exercises/Part 5/Exercise 123/Program.cs
@@ -7,8 +7,22 @@ namespace exercise_123
     public static void Main(string[] args)
     {
       // You can test your code here.
-      Counter counter = new Counter();
-      
+      Counter counter1 = new Counter(0);
+      Counter counter2 = new Counter(10);
+            
+      Console.WriteLine("Counter 1:");
+      counter1.Increase();
+      counter1.Increase();
+      counter1.Decrease();
+      counter1.Decrease(-10);
+      Console.WriteLine(counter1.value);
+      Console.WriteLine();
+      Console.WriteLine("Counter 2:");
+      counter2.Increase(-10);
+      counter2.Increase(5);
+      counter2.Decrease(2);
+      Console.WriteLine(counter2.value);
+      Console.WriteLine();
     }
   }
 }
